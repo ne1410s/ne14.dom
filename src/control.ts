1ecdd5f4d72c066eaf72a789d2f5abd2baaf48f6
Paul Jones <paul.jones@creditplus.co.uk>
2019-02-26 10:28:24 +0000

    Utillising "references" over imports for more browser-friendly approach.

--- src/control.ts
+++ src/control.ts
@@ -1,5 +1,11 @@
-export abstract class Control {
+abstract class Control {
     
+    /**
+     * Toggles a class for a given element.
+     * @param elem The element.
+     * @param cn The class name.
+     * @param val Optional override to force add/removal.
+     */
     public static toggleClass(elem: Element, cn: string, val?: boolean): void {
 
         const doAdd = val == null ? !elem.classList.contains(cn) : val,
--- src/query.ts
+++ src/query.ts
@@ -1,16 +1,26 @@
-export abstract class Query {
+abstract class Query {
     
-    public static array(query: string, elem?: Element): Array<Element> {
+    /**
+     * Returns an array of elements that match the css-style query.
+     * @param query The css-style query.
+     * @param root The starting point of the search.
+     */
+    public static array(query: string, root?: Element): Array<Element> {
 
-        const parent = elem || document,
+        const parent = root || document,
               nodes = parent.querySelectorAll(query);
 
         return Array.from(nodes);
     }
 
-    public static first(query: string, elem?: Element): Element {
+    /**
+     * Returns the first element that matches the css-style query.
+     * @param query The css-style query.
+     * @param root The starting point of the search.
+     */
+    public static first(query: string, root?: Element): Element {
 
-        const parent = elem || document,
+        const parent = root || document,
               node = parent.querySelector(query);
 
         return node;
