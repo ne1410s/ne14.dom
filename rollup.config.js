6ea9e6ae28e477fbb2675332cab9f30274232dcf
Paul Jones <pjjones@siemens.com>
2020-02-14 10:56:12 +0000

    Streamlined build into single rollup process

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,28 @@
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default [
 
   { // UMD build (for browsers)
-    input: 'dist/index.js',
+    input: 'src/index.ts',
     output: {
       name: 'ne_dom',
       file: pkg.browser,
       format: 'umd'
-    }
+    },
+    plugins: [
+      typescript()
+    ]
   },
 
   { // CommonJS (for Node) and ES module (for bundlers) builds
-    input: 'dist/index.js',
+    input: 'src/index.ts',
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      typescript()
     ]
   }
 ];
