f2ba6699707ad969869d9c82f8e9ac30a451450d
ne1410s <pgj646@gmail.com>
2020-03-26 21:15:10 +0000

    Throttle and debounce pending integration

--- src/functions.ts
+++ src/functions.ts
@@ -10,3 +10,31 @@ export function ready(func: () => void): void {
 export function q(...input: (string | QuickParam | Element)[]): ChainedQuery {
   return new ChainedQuery(...input);
 }
+
+// TODO: Move throttle and debounce to chained-query...
+// However, this needs the backing array to accept additional object type(s)
+// For example ne_dom.q(window).debounce('resize' () => console.log('resized'))
+
+// export function throttle<T>(func: (arg: T) => void, delay = 200): (arg: T) => void {
+//   let active: boolean;
+//   return function (args) {
+//     if (!active) {
+//       if (active == null) func.call(this, args);
+//       active = true;
+//       const that = this;
+//       setTimeout(() => { 
+//         active = !!func.call(that, args);
+//         setTimeout(() => active = active || null, delay / 10);
+//       }, delay);
+//     }
+//   };
+// }
+
+// export function debounce<T>(func: (arg: T) => void, delay = 200): (arg: T) => void {
+//   let timeout: NodeJS.Timeout;
+//   return function (arg) {
+//     clearTimeout(timeout);
+//     const that = this;
+//     timeout = setTimeout(() => func.call(that, arg), delay);
+//   };
+// }
